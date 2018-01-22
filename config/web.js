79e096f526f2d3fda144261e34e126a84037af18
Guillaume Viguier <guillaume@viguierjust.com>
2018-01-22 16:00:20 +0100

    Removed x-powered-by

--- config/web.js
+++ config/web.js
@@ -15,25 +15,5 @@ module.exports = {
       // html: require('some-view-engine')
     },
     path: 'views'
-  },
-
-  extensions: [
-    {
-      type: 'onPreResponse',
-      method (request, reply) {
-        const trailsVersion = this.app['_trails'].version
-        const nodeVersion = this.app.versions.node
-        const poweredBy = `Node/${nodeVersion} Trails/${trailsVersion}`
-
-        if (request.response.isBoom) {
-          request.response.output.headers['X-Powered-By'] = poweredBy
-        }
-        else if (request.response.header) {
-          request.response.header('X-Powered-By', poweredBy)
-        }
-
-        reply.continue()
-      }
-    }
-  ]
+  }
 }
